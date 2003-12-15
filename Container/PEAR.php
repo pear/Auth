0df0d1d9f75bf33f644a744fe743c8fcba82cb8a
Yavor Shahpasov <yavo@php.net>
2003-12-15 23:51:57 +0000

    A auth container to use the pear web site authenthication
    user xml_rpc call to user.info method
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/Auth/trunk@146534 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Container/PEAR.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('XML/RPC.php');
+
+class Auth_Container_Pear {
+    function Auth_Container_Pear(){
+    
+    }
+    
+    function fetchData($username, $password){
+        $rpc = new XML_RPC_Client('/xmlrpc.php', 'pear.php.net');
+        $rpc_message = new XML_RPC_Message("user.info", array(new XML_RPC_Value($username, "string")) );
+        // Error Checking howto ???
+        $result = $rpc->send($rpc_message);
+        $value = $result->value();
+        $userinfo = xml_rpc_decode($value);
+        if($userinfo['password'] == md5($password)){
+            $this->activeUser = $userinfo['handle'];
+            foreach($userinfo as $uk=>$uv){
+                $this->_auth_obj->setAuthData($uk, $uv);
+            }
+            return(true);
+        }
+        return(false);
+    }
+    
+}
+
+?>
