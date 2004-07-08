39b844ee7bf2c9cae831b42115cfc303f84f6443
Yavor Shahpasov <yavo@php.net>
2004-07-08 11:22:23 +0000

    A class to allow anonymous authenthication
    git-svn-id: http://svn.php.net/repository/pear/packages/Auth/trunk@162863 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Auth/Anonymous.php
@@ -0,0 +1,51 @@
+<?php
+
+
+include_once 'Auth.php';
+
+/**
+ * This class provides anonymous authenthication
+ * if username and password were not supplied
+ *
+ */
+class Auth_Anonymous extends Auth {
+
+    var $allow_anonymous = true;
+    var $anonymous_username = 'anonymous';
+    
+    /**
+      * Pass all parameters to Parent Auth class
+      */
+    function Auth_Anonymous($storageDriver, $options = '', $loginFunction = '', $showLogin = true) {
+        parent::Auth($storageDriver, $options, $loginFunction, $showLogin);
+    }
+    
+    /**
+      * If no username & password is passed
+      * then login the user as anonymous
+      */
+    function login() {
+        if ( $this->allow_anonymous && empty($this->username) && empty($this->password) ) {
+            $this->setAuth($this->anonymous_username);
+            if (is_callable($this->loginCallback)) {
+                call_user_func_array($this->loginCallback, array($this->username, &$this) );
+            }
+        } else {
+            print "Real Login";
+            parent::login();
+        }
+    }
+    
+    /**
+      * Force the user to login
+      *
+      */
+    function forceLogin() {
+        $this->allow_anonymous = false;
+        if( !empty($this->session['username']) && $this->session['username'] == $this->anonymous_username ) {
+            $this->logout();
+        }
+    }
+}
+
+?>
