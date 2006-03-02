7810e449a6e7449ee8c8f8296377b160e15654f3
Adam Ashley <aashley@php.net>
2006-03-02 02:53:03 +0000

    extend PEAR authentication container from Auth_Container properly. documentation updates
    git-svn-id: http://svn.php.net/repository/pear/packages/Auth/trunk@208425 c90b9560-bf6c-de11-be94-00142212c4b1

--- Container/PEAR.php
+++ Container/PEAR.php
@@ -1,26 +1,35 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Martin Jansen <mj@php.net>                                  |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
 
-/** Include PEAR XML_RPC */
-require_once('XML/RPC.php');
+/**
+ * Auth_Container Base Class
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Authentication
+ * @package    Auth
+ * @author     Yavor Shahpasov <yavo@netsmart.com.cy>
+ * @author     Adam Ashley <aashley@php.net>
+ * @copyright  2001-2005 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Auth
+ */
+
+/**
+ * Include Auth_Container base class
+ */
+require_once 'Auth/Container.php';
+/**
+ * Include PEAR XML_RPC
+ */
+require_once 'XML/RPC.php';
 
 /**
  * Storage driver for authenticating against PEAR website
@@ -28,12 +37,16 @@ require_once('XML/RPC.php');
  * This driver provides a method for authenticating against the pear.php.net
  * authentication system.
  *
- * @author   Yavor Shahpasov <yavo@netsmart.com.cy>
- * @author   Adam Ashley <aashley@php.net>
- * @package  Auth
- * @version  $Revision$
+ * @category   Authentication
+ * @package    Auth
+ * @author     Yavor Shahpasov <yavo@netsmart.com.cy>
+ * @author     Adam Ashley <aashley@php.net>
+ * @copyright  2001-2005 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Auth
  */
-class Auth_Container_Pear
+class Auth_Container_Pear extends Auth_Container
 {
 
     // {{{ Auth_Container_Pear() [constructor]
