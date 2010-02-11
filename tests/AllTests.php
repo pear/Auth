22df69f97d10296c536cd142297511230683421a
Daniel O'Connor <clockwerx@php.net>
2010-02-11 22:45:05 +0000

    PHPUnit 3.4 compatibility
    git-svn-id: http://svn.php.net/repository/pear/packages/Auth/trunk@294909 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -3,7 +3,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Auth_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 require_once 'DBContainer.php';
